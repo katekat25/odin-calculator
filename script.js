0dd3edeb8ea5685f56dee509daf556b9f3645028
Kate <kate.schumacher@gmail.com>
2024-09-27 12:36:52 -0500

    Remove comments

--- script.js
+++ script.js
@@ -10,7 +10,6 @@ const creditButton = document.querySelector("#credit");
 numButtons.forEach((button) => {
     button.addEventListener("click", () => {
         let buttonValue = button.textContent;
-        // console.log("Button clicked is " + buttonValue);
         displayValue.textContent += buttonValue;
     });
 })
@@ -54,47 +53,32 @@ function calculate(string) {
         || string.includes("-")) {
         let arr = string.split(" ");
         let numberOfOperators = arr.filter(x => x == "X" || x == "/" || x == "+" || x == "-").length;
-        // console.log("numberOfOperators is " + numberOfOperators);
         for (let i = 0; i < numberOfOperators; i++) {
-            // console.log("In the god forsaken for loop. " + i + "st round.");
             if (arr.includes("X") || arr.includes("/")) {
-                // console.log("Array includes X or /. Calculating those first.")
                 if (arr.indexOf("X") == -1) {
-                    // console.log("There is no X in this equation.");
                     arr = calculateChunk("/", arr);
                 } else if (arr.indexOf("/") == -1) {
-                    // console.log("There is no / in this equation.");
                     arr = calculateChunk("X", arr);
                 } else arr = arr.indexOf("X") < arr.indexOf("/") ? calculateChunk("X", arr) : calculateChunk("/", arr);
-                // console.log("New array is " + arr);
             }
             else if (arr.includes("+") || arr.includes("-")) {
-                // console.log("Array includes + or -. Calculating those now.")
                 if (arr.indexOf("+") == -1) {
-                    // console.log("There is no + in this equation.");
                     arr = calculateChunk("-", arr);
                 } else if (arr.indexOf("-") == -1) {
-                    // console.log("There is no - in this equation.");
                     arr = calculateChunk("+", arr);
                 } else arr = arr.indexOf("+") < arr.indexOf("-") ? calculateChunk("+", arr) : calculateChunk("-", arr);
-                // console.log("New array is " + arr);
             }
 
         }
-        // console.log("Exited loop. Returning array[0], " + arr[0])
         return arr[0];
     }
 }
 
 function calculateChunk(operator, array) {
-    // console.log("In calculateChunk. Array is " + array + " and operator is " + operator);
     let indexOfOperator = array.indexOf(operator);
-    // console.log("indexOfOperator is " + indexOfOperator);
     array[indexOfOperator] = operate(operator, array[indexOfOperator - 1], array[indexOfOperator + 1]);
-    // console.log("We attempted to operate the thing at index " + indexOfOperator + ". Now it's " + array[indexOfOperator]);
     array.splice(indexOfOperator + 1, 1);
     array.splice(indexOfOperator - 1, 1);
-    // console.log("Modified array we're returning now is " + array);
     return array;
 }
 
@@ -119,12 +103,11 @@ function operate(operator, a, b) {
             return divide(a, b);
             break;
         default:
-            console.log("Some kind of wacky thang happened oops.");
+            console.log("Some kind of wacky thing happened oops.");
     }
 }
 
 function add(a, b) {
-    console.log("Added a and b. I got " + (Number(a) + Number(b)));
     return Number(a) + Number(b);
 }
 
